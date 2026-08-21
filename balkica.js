5b32d952dab8a538c38bb75ee89ebfce61e90168
Nernur <bilalkamera20@gmail.com>
2026-08-21 12:04:38 +0300

    Change GROUP to 'Turkey' and update filter
    Updated GROUP constant to 'Turkey' and modified filter to an empty object.

--- balkica.js
+++ balkica.js
@@ -4,7 +4,7 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 
 const CATALOG_URL = "https://vavoo.to/mediahubmx-catalog.json";
-const GROUP = "";
+const GROUP = "Turkey";
 // Dosya çıkışı 'nernur.txt' olarak güncellendi
 const M3U_FILE = path.join(__dirname, "nernur.txt");
 const FETCH_TIMEOUT_MS = 20000;
@@ -67,7 +67,7 @@ function buildBody(cursor) {
     adult: false,
     search: "",
     sort: "name",
-    filter: { group: GROUP },
+    filter: { },
     cursor,
   });
 }
