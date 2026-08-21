666a5cff8fe3d4b124e89e34de5a30057d7c4eba
Nernur <bilalkamera20@gmail.com>
2026-08-21 12:02:42 +0300

    Change GROUP constant to an empty string

--- balkica.js
+++ balkica.js
@@ -4,7 +4,7 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 
 const CATALOG_URL = "https://vavoo.to/mediahubmx-catalog.json";
-const GROUP = "Turkey";
+const GROUP = "";
 // Dosya çıkışı 'nernur.txt' olarak güncellendi
 const M3U_FILE = path.join(__dirname, "nernur.txt");
 const FETCH_TIMEOUT_MS = 20000;
